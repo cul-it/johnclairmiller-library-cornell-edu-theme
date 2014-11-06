74abc44e0577528835bb4f48dc9bb4df6c9ebb69
Melissa Wallace <mhk33@cornell.edu>
2014-11-06 13:43:05 -0500

    finish up the if's

--- templates/node--work.tpl.php
+++ templates/node--work.tpl.php
@@ -7,44 +7,6 @@
  * @see https://drupal.org/node/1728164
  */
 ?>
-<!-- <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
-    <header>
-      <?php print render($title_prefix); ?>
-      <?php if (!$page && $title): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-
-      <?php if ($display_submitted): ?>
-        <p class="submitted">
-          <?php print $user_picture; ?>
-          <?php print $submitted; ?>
-        </p>
-      <?php endif; ?>
-
-      <?php if ($unpublished): ?>
-        <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
-
-  <p>TEST PAGE</p>
-
-  <?php
-    // We hide the comments and links now so that we can render them later.
-    hide($content['comments']);
-    hide($content['links']);
-    print render($content);
-  ?>
-
-  <?php print render($content['links']); ?>
-
-  <?php print render($content['comments']); ?>
-
-</article> -->
-
 <div class="row">
   <div class="access">
     <?php print render($content['field_work_image']); ?>
@@ -53,32 +15,50 @@
   </div>
   <div class="work-data">
     <dl>
-      <?php if ($content['type']) : ?>
+      <?php if ($content['field_type']) : ?>
         <dt>Type</dt>
         <dd><?php print render($content['field_type']); ?></dd>
       <?php endif; ?>
-      <dt>Location</dt>
-      <dd><?php print render($content['field_location']); ?></dd>
-      <dt>Owner</dt>
-      <dd><?php print render($content['field_owner']); ?></dd>
+      <?php if ($content['field_location']) : ?>
+        <dt>Location</dt>
+        <dd><?php print render($content['field_location']); ?></dd>
+      <?php endif; ?>
+      <?php if ($content['field_owner']) : ?>
+        <dt>Owner</dt>
+        <dd><?php print render($content['field_owner']); ?></dd>
+      <?php endif; ?>
       <?php if ($content['sponsor']) : ?>
         <dt>Sponsor</dt>
         <dd><?php print render($content['field_sponsor']); ?></dd>
       <?php endif; ?>
-      <dt>Date</dt>
-      <dd><?php print render($content['field_date']); ?></dd>
-      <dt>Design</dt>
-      <dd><?php print render($content['field_design']); ?></dd>
-      <dt>Engineering</dt>
-      <dd><?php print render($content['field_engineering']); ?></dd>
-      <dt>Award</dt>
-      <dd><?php print render($content['field_award']); ?></dd>
+      <?php if ($content['field_date']) : ?>
+        <dt>Date</dt>
+        <dd><?php print render($content['field_date']); ?></dd>
+      <?php endif; ?>
+      <?php if ($content['field_design']) : ?>
+        <dt>Design</dt>
+        <dd><?php print render($content['field_design']); ?></dd>
+      <?php endif; ?>
+      <?php if ($content['field_engineering']) : ?>
+        <dt>Engineering</dt>
+        <dd><?php print render($content['field_engineering']); ?></dd>
+      <?php endif; ?>
+      <?php if ($content['field_award']) : ?>
+        <dt>Award</dt>
+        <dd><?php print render($content['field_award']); ?></dd>
+      <?php endif; ?>
     </dl>
-    <h2>Program Description</h2>
-    <?php print render($content['field_program']); ?>
-    <h2>Design Description</h2>
-    <?php print render($content['field_design_description']); ?>
-    <h2>Award Description</h2>
-    <?php print render($content['field_award_description']); ?>
+    <?php if ($content['field_program']) : ?>
+      <h2>Program Description</h2>
+      <?php print render($content['field_program']); ?>
+    <?php endif; ?>
+    <?php if ($content['field_design_description']) : ?>
+      <h2>Design Description</h2>
+      <?php print render($content['field_design_description']); ?>
+    <?php endif; ?>
+    <?php if ($content['field_award_description']) : ?>
+      <h2>Award Description</h2>
+      <?php print render($content['field_award_description']); ?>
+    <?php endif; ?>
   </div>
 </div>
