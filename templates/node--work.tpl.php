f7b524cefd02d823f1c2c8a2d34e73c1108a606f
Melissa Wallace <mhk33@cornell.edu>
2014-11-07 11:45:13 -0500

    fix ifs for fields to make undefined index error go away i hope

--- templates/node--work.tpl.php
+++ templates/node--work.tpl.php
@@ -23,40 +23,40 @@
         <dt>Location</dt>
         <dd><?php print render($content['field_location']); ?></dd>
       <?php endif; ?>
-      <?php if ($content['field_owner']) : ?>
+      <?php if (!empty ($content['field_owner'])) : ?>
         <dt>Owner</dt>
         <dd><?php print render($content['field_owner']); ?></dd>
       <?php endif; ?>
-      <?php if ($content['field_sponsor']) : ?>
+      <?php if (!empty ($content['field_sponsor'])) : ?>
         <dt>Sponsor</dt>
         <dd><?php print render($content['field_sponsor']); ?></dd>
       <?php endif; ?>
-      <?php if ($content['field_date']) : ?>
+      <?php if (!empty ($content['field_date'])) : ?>
         <dt>Date</dt>
         <dd><?php print render($content['field_date']); ?></dd>
       <?php endif; ?>
-      <?php if ($content['field_design']) : ?>
+      <?php if (!empty ($content['field_design'])) : ?>
         <dt>Design</dt>
         <dd><?php print render($content['field_design']); ?></dd>
       <?php endif; ?>
-      <?php if ($content['field_engineering']) : ?>
+      <?php if (!empty ($content['field_engineering'])) : ?>
         <dt>Engineering</dt>
         <dd><?php print render($content['field_engineering']); ?></dd>
       <?php endif; ?>
-      <?php if ($content['field_award']) : ?>
+      <?php if (!empty ($content['field_award'])) : ?>
         <dt>Award</dt>
         <dd><?php print render($content['field_award']); ?></dd>
       <?php endif; ?>
     </dl>
-    <?php if ($content['field_program']) : ?>
+    <?php if (!empty ($content['field_program'])) : ?>
       <h2>Program Description</h2>
       <?php print render($content['field_program']); ?>
     <?php endif; ?>
-    <?php if ($content['field_design_description']) : ?>
+    <?php if (!empty ($content['field_design_description'])) : ?>
       <h2>Design Description</h2>
       <?php print render($content['field_design_description']); ?>
     <?php endif; ?>
-    <?php if ($content['field_award_description']) : ?>
+    <?php if (!empty ($content['field_award_description'])) : ?>
       <h2>Award Description</h2>
       <?php print render($content['field_award_description']); ?>
     <?php endif; ?>
