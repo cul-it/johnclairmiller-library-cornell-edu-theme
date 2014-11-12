a02ee61a549c3b97873aa2d1dd9e22dbc301f621
Melissa Wallace <mhk33@cornell.edu>
2014-11-12 11:15:11 -0500

    remove button style

--- templates/node--work.tpl.php
+++ templates/node--work.tpl.php
@@ -10,8 +10,8 @@
 <div class="row">
   <div class="access">
     <?php print render($content['field_work_image']); ?>
-    <a href="http://catalog.sharedshelf.artstor.org" class="btn btn-jcm">Browse images of <?php print $title; ?><i class="fa fa-external-link"></i></a>
-    <a href="http://catalog.sharedshelf.artstor.org" class="btn btn-jcm">Browse all images<i class="fa fa-external-link"></i></a>
+    <a href="http://catalog.sharedshelf.artstor.org">Browse images of <?php print $title; ?><i class="fa fa-external-link"></i></a>
+    <a href="http://catalog.sharedshelf.artstor.org">Browse all images<i class="fa fa-external-link"></i></a>
   </div>
   <div class="work-data">
     <dl>
